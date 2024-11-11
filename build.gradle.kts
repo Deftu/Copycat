c424d8e9537937eba2660a8070a76a3a34d9fa87
LynithDev <61880709+LynithDev@users.noreply.github.com>
2024-11-11 20:19:28 +0100

    tmp: ignore tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,17 @@ dependencies {
 
     rootProject.subprojects.filter { project ->
         project.name.startsWith("natives-")
-    }.forEach(::testImplementation)
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    }
+//        .forEach(::testImplementation)
+//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks {
 
-    test {
-        useJUnitPlatform()
-        ignoreFailures = System.getenv("CI") == "true"
-    }
+//    test {
+//        useJUnitPlatform()
+//        ignoreFailures = System.getenv("CI") == "true"
+//    }
 
 }
